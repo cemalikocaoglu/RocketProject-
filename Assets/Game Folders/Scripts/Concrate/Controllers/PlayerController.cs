88f5c196cb0272b320a57fc5c2c3874fd9aa284b
cemalikocaoglu <83187104+cemalikocaoglu@users.noreply.github.com>
2023-06-30 01:52:05 +0300

    Player Controllers adding

---
+++ Assets/Game Folders/Scripts/Concrate/Controllers/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RpcketProject.PlayerControllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        
+    }
+}
